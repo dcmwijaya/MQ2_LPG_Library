7ba5dda7bf43a8008ac33498bda32d19156f690e
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-22 13:49:28 +0700

    Update basic_mq2_lpg.ino

--- example/basic_mq2_lpg/basic_mq2_lpg.ino
+++ example/basic_mq2_lpg/basic_mq2_lpg.ino
@@ -19,7 +19,7 @@ void setup() {
 #define y2_Value 0.8990240080541785
 #define x_Value 497.4177875376839
 #define y_Value 1.0876679972710004
-#define Ro_Value 4.26
+#define Ro_Value 6.31
 #define Voltage_Value 5.0
 #define bitADC_Value 1023.0
 
